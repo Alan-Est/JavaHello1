62ea299ba4519b0e0e7e6a03bd85d716b7fb8b25
Alan-Est <59730414+Alan-Est@users.noreply.github.com>
2020-01-10 20:12:23 +0800

    Create students.sql

---
+++ students.sql
@@ -0,0 +1,5 @@
+INSERT INTO `students` VALUES ('125', 0, 15, 5, 1, 1, 1);
+INSERT INTO `students` VALUES ('225', 1, 2, 5, 25, 2, 2);
+INSERT INTO `students` VALUES ('5', 4, 5, 5, 5, 5, 59);
+INSERT INTO `students` VALUES ('69', 5, 6, 5, 6, 6, 9);
+INSERT INTO `students` VALUES ('1', 6, 1, 1, 1, 1, 1);
